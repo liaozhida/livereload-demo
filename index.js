5387985a84d603c2c4d918c8ded6eeea4015e57e
廖志达 <zhida.liao@yeamoney.cn>
2016-06-16 09:35:32 +0800

    l

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ var PORT = process.env.PORT || 1337;
 var reloadify = require('./lib/reloadify');
 reloadify(app, __dirname + '/views');
 
-// view engine setup
 app.engine('html', ejs.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-// serve an empty page that just loads the browserify bundle
 app.get('/', function(req, res) {
 	res.render('home');
 });
